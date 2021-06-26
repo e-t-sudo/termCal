c13a84b64e481a3cf190c1a5bbfc7077ece4a035
e_t <77915530+e-t-sudo@users.noreply.github.com>
2021-06-26 22:49:14 +0530

    Wait for the page to load

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
- if (document.readyState === "complete"){
-    if(sessionStorage.getItem("color")=="dark"){
+if(sessionStorage.getItem("color")=="dark"){
         document.querySelector(':root').style.setProperty('--textCol', '#f3f3f3');
         document.querySelector(':root').style.setProperty('--backCol', '#000000');
-    }
-
-    document.getElementById('input_expr').onkeypress = function(key){
-        if(key.keyCode == 13){ //when "enter" is pressed   
+   }
+        document.addEventListener("DOMContentLoaded", function(){
+        document.getElementById('input_expr').onkeypress = function(key){
+        if(key.keyCode == 13){ //when "enter" is pressed
                 let evaluated_expr = document.getElementById('input_expr').value;
                 evaluated_expr = evaluated_expr.replace(/^ +| +$/g, ''); //trims off all the leading and trailing whitespaces
                 if(/^\/\//.test(evaluated_expr)){
@@ -15,17 +14,17 @@
                     return;
                 }
                 evaluated_expr = evaluated_expr.replace(/ /g, ''); //replaces all whitespaces by "" empty character
-                let arr = evaluated_expr.split(""); 
+                let arr = evaluated_expr.split("");
                 if(evaluated_expr=="clear"){ //clear directive
-                    document.getElementById('yang').innerHTML=""; 
-                    document.getElementById('input_expr').value = ""; 
+                    document.getElementById('yang').innerHTML="";
+                    document.getElementById('input_expr').value = "";
                 }else if(evaluated_expr=="history"){
                     if(INPUTS.length==0){//no history to show
                         document.getElementById('yang').innerHTML += "<p>[-y/y] : Empty log!</p>";
                     }else{
                        document.getElementById('yang').innerHTML += "<p>history</p>";
                         for(let i=0; i<INPUTS.length; i++){
-                            let inputStr = INPUTS[i].join(""); 
+                            let inputStr = INPUTS[i].join("");
                             let outputStr = OUTPUTS[i];
                             document.getElementById('yang').innerHTML += "<p style='color:grey'>"+inputStr+" = "+outputStr+"</p>";
                         }
@@ -50,15 +49,15 @@
                             }
                             document.getElementById('input_expr').value = response;
                  }else{ //commands other than clear
-                    let new_p = "<p>" + evaluated_expr + "</p>"; 
-                    document.getElementById('yang').innerHTML += new_p; 
-                    let result = eval(arr); 
+                    let new_p = "<p>" + evaluated_expr + "</p>";
+                    document.getElementById('yang').innerHTML += new_p;
+                    let result = eval(arr);
                     document.getElementById('yang').innerHTML += (result!=undefined)?result:"[-y/y] : '"+evaluated_expr+"' operation not defined..."; //eval() is defined in the './myFile.js' module
                     document.getElementById('input_expr').value = "";
                 }
                 let cmdNo = INPUTS.length + 1;
                 document.getElementById('yin').innerText = "[" + cmdNo + "]:";
-                document.getElementById('input_expr').scrollIntoView(); 
+                document.getElementById('input_expr').scrollIntoView();
         }
     }
- }
+});
